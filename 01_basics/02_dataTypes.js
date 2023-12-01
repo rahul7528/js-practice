737a31c4e6ffebeee90d848839c047f320262ef1
rahul7528 <152489944+rahul7528@users.noreply.github.com>
2023-12-01 08:35:20 +0000

    Information on dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,42 @@
-console.log('something');
-console.log('something');
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// Learnings
+
+/*
+: dataTypes - number, string, boolean, null, undefined, symbols, bigint.
+: Difference between the dataTypes.
+: bigint - Big numeric value which can't be print in numbers. 
+example - 10000000000000*10000000000000 (all numbers after 20 digit, under 20 its number)
+: null vs undefined.
+: null - standalone value (let temp= null, we cannot put 0 as a value because temp 0 is a temp, so we have to return null, incase of no value)
+: typeof
+: convert string to number
+*/
