70ce69441e32350ff91d331d0ba596dcc09e89eb
rahul7528 <152489944+rahul7528@users.noreply.github.com>
2023-12-06 16:15:36 +0000

    added comments to min&mx chapter

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -24,11 +24,22 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
 
+console.log(Math.random());
+// to get random numbers
+// Math.random() gives number between 0-0.99
+console.log((Math.random()*10) + 1);
+// to get number between 1-9 we have to multiply it with 10 (0.1 * 10 = 1)
+// there are chances to get 0 in random and 0*10 = 0 therefore we have to add 1
+console.log(Math.floor(Math.random()*10) + 1);
+// Math.floor to round off the values and get a single digit.
+
+// Trick
 const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// Generate random Number
+// (max - min + 1) (20-10 + 1) (+ 1 to avoid 0)
+// + min to atleast add min value 
