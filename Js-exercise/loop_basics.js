6fb36344bf5848619db3b7abf961e2eb5074f0b4
rahulChawla <rahul.chawla7528@gmail.com>
2024-01-25 04:28:26 +0530

    loop exercise

---
+++ Js-exercise/loop_basics.js
@@ -0,0 +1,15 @@
+// There are 6 types of loop in Javascript:
+
+// for : Basic loop that can be used for arrays, strings, or other iterable objects.
+   
+// while : It executes a block of code as long as a specified condition is true.
+
+// do-while : Similar to the while loop, but it always executes the block of code at least once before checking the condition.
+
+// for...in : Specifically designed for iterating over the properties of an object. imp - It gives key on iteration
+
+// for...of : Introduced in ECMAScript 2015 (ES6), it is used for iterating over iterable objects, like arrays or strings. It provides a more concise syntax compared to the traditional for loop. imp - It gives value on iteration.
+
+// forEach method (for arrays): A method available for arrays that executes a provided function once for each array element.
+
+// Remember, while the `for...of` loop and `forEach` method are often used with arrays, the `for loop` can also be used for iterating over arrays, strings, or any other iterable data structure. The choice often depends on the specific requirements of the task at hand.
